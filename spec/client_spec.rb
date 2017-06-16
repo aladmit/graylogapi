365efa62d39b761d6048475c5498a6b3fe2ecf63
Andrey Aleksandrov <postgred@gmail.com>
2017-06-17 01:06:13 +0300

    move client to shared_context
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,19 +1,15 @@
 describe 'client' do
-  before do
-    @client = Graylogapi::Client.new('http://localhost:9000/api',
-                                     'admin',
-                                     'admin')
-  end
+  include_context 'client'
 
   it 'should have base_uri' do
-    expect(@client.class.base_uri).to eq 'http://localhost:9000/api'
+    expect(client.class.base_uri).to eq 'http://localhost:9000/api'
   end
 
   it 'should have username' do
-    expect(@client.class.default_options[:basic_auth][:username]).to eq 'admin'
+    expect(client.class.default_options[:basic_auth][:username]).to eq 'admin'
   end
 
   it 'should have password' do
-    expect(@client.class.default_options[:basic_auth][:password]).to eq 'admin'
+    expect(client.class.default_options[:basic_auth][:password]).to eq 'admin'
   end
 end
