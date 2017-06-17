f3ca42b4228ffc9e4455202a0f6cf309709e372d
Andrey Aleksandrov <postgred@gmail.com>
2017-06-17 16:13:54 +0300

    use net/http instead httparty
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- lib/graylogapi/client.rb
+++ lib/graylogapi/client.rb
@@ -1,18 +1,54 @@
-require 'httparty'
+require 'net/http'
+require 'uri'
+require 'json'
 
 module Graylogapi
   # The client is the entry point to the api
   class Client
-    include HTTParty
+    attr_reader :options
 
-    def initialize(api_url, user, pass)
-      self.class.base_uri api_url
-      self.class.default_options.merge!(basic_auth: { username: user,
-                                                      password: pass })
+    def initialize(options = {})
+      @options = options
+      uri = URI.parse(options[:base_url])
+      @http = Net::HTTP.new(uri.host, uri.port)
+    end
+
+    def get(url, params)
+      json_request(:get, url, params)
+    end
+
+    def post(url, body)
+      json_request(:post, url, body)
     end
 
     def alerts
-      @alerts ||= Alerts.new
+      @alerts ||= Alerts.new(self)
+    end
+
+    private
+
+    def json_request(method, path, params = {})
+      request = request(method, path, params)
+      request.basic_auth(options[:user], options[:pass])
+      request.add_field('Content-Type', 'application/json')
+      JSON.parse(@http.request(request).body)
+    end
+
+    def request(method, path, params = {})
+      case method
+      when :get
+        full_path = encode_path_params(path, params)
+        request = Net::HTTP::Get.new(full_path)
+      else
+        request = Net::HTTP::Post.new(options[:base_url] + path)
+        request.body = params.to_json
+      end
+      request
+    end
+
+    def encode_path_params(path, params)
+      encoded = URI.encode_www_form(params)
+      [path, encoded].join('?')
     end
   end
 end
