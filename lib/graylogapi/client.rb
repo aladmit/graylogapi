2eaafe610b1f14489bd8faa4582349ebe0231f24
Andrey Aleksandrov <postgred@gmail.com>
2017-06-18 17:53:42 +0300

    Add all HTTP methods
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- lib/graylogapi/client.rb
+++ lib/graylogapi/client.rb
@@ -17,18 +17,30 @@ module Graylogapi
       json_request(:get, url, params)
     end
 
-    def post(url, body = {})
-      json_request(:post, url, body)
+    def post(url, params = {})
+      json_request(:post, url, params)
+    end
+
+    def put(url, params = {})
+      json_request(:put, url, params)
+    end
+
+    def delete(url, params = {})
+      json_request(:delete, url, params)
     end
 
     def alerts
       @alerts ||= Alerts.new(self)
     end
 
+    def system
+      @system ||= System.new(self)
+    end
+
     private
 
     def json_request(method, path, params = {})
-      request = request(method, path, params)
+      request = method("#{method}_request").call(path, params)
       request.basic_auth(options[:user], options[:pass])
       request.add_field('Content-Type', 'application/json')
       response = @http.request(request)
@@ -38,21 +50,26 @@ module Graylogapi
       response
     end
 
-    def request(method, path, params = {})
-      case method
-      when :get
-        full_path = encode_path_params(options[:base_url] + path, params)
-        request = Net::HTTP::Get.new(full_path)
-      else
-        request = Net::HTTP::Post.new(options[:base_url] + path)
-        request.body = params.to_json
-      end
-      request
+    def get_request(path, params = {})
+      full_path = [options[:base_url] + path, URI.encode_www_form(params)]
+      Net::HTTP::Get.new(full_path.join('?'))
     end
 
-    def encode_path_params(path, params)
-      encoded = URI.encode_www_form(params)
-      [path, encoded].join('?')
+    def post_request(path, params = {})
+      req = Net::HTTP::Post.new(options[:base_url] + path)
+      req.body = params.to_json
+      req
+    end
+
+    def put_request(path, params = {})
+      req = Net::HTTP::Put.new(options[:base_url] + path)
+      req.body = params.to_json
+      req
+    end
+
+    def delete_request(path, params = {})
+      full_path = [options[:base_url] + path, URI.encode_www_form(params)]
+      Net::HTTP::Delete.new(full_path.join('?'))
     end
   end
 end
