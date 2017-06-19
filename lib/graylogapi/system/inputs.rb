4f8b46358edeb720520491dd3c116387c25992b5
Andrey Aleksandrov <postgred@gmail.com>
2017-06-19 20:34:05 +0300

    add doc
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- lib/graylogapi/system/inputs.rb
+++ lib/graylogapi/system/inputs.rb
@@ -6,22 +6,40 @@ class GraylogAPI
         @client = client
       end
 
+      # get all inputs
+      #
+      # @return [Struct]
       def all
         @client.get('/system/inputs')
       end
 
+      # get input by input id
+      #
+      # @return [Struct]
       def by_id(id)
         @client.get("/system/inputs/#{id}")
       end
 
+      # create input
+      #
+      # @param params [Hash]
+      # @return [Struct]
       def create(params = {})
         @client.post('/system/inputs', params)
       end
 
+      # update input
+      #
+      # @param params [Hash]
+      # @return [Struct]
       def update(id, params = {})
         @client.put("/system/inputs/#{id}", params)
       end
 
+      # delete input
+      #
+      # @param params [Hash]
+      # @return [Struct]
       def delete(id, params = {})
         @client.delete("/system/inputs/#{id}", params)
       end
