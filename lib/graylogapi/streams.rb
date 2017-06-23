17b4e740c7517f77c40e46026532bd8e74532855
Andrey Aleksandrov <postgred@gmail.com>
2017-06-23 23:18:31 +0300

    all, enabled, by_id for streams
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- lib/graylogapi/streams.rb
+++ lib/graylogapi/streams.rb
@@ -5,8 +5,20 @@ class GraylogAPI
       @client = client
     end
 
+    def all
+      @client.json_request(:get, '/streams')
+    end
+
     def create(params = {})
       @client.json_request(:post, '/streams', params)
+    end
+
+    def enabled
+      @client.json_request(:get, '/streams/enabled')
+    end
+
+    def by_id(id)
+      @client.json_request(:get, "/streams/#{id}")
     end
 
     def delete(id, params = {})
