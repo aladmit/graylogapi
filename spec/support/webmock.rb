0311c5e6784d03b2eddbe4714e0860768c97da69
Andrey Aleksandrov <postgred@gmail.com>
2017-08-28 13:44:10 +0300

    fix rubocop
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -6,8 +6,8 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
 
-  record_mode = ENV["VCR"] ? ENV["VCR"].to_sym : :once
-  c.default_cassette_options = { :record => record_mode }
+  record_mode = ENV['VCR'] ? ENV['VCR'].to_sym : :once
+  c.default_cassette_options = { record: record_mode }
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
