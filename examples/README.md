b3ae02e7d01a3f31904b17821cef612ce9e7fcbf
Andrey Aleksandrov <postgred@Andreys-MacBook-Pro.local>
2017-12-24 00:45:59 +0300

    clean old code

--- examples/README.md
+++
@@ -1,8 +0,0 @@
-# GraylogAPI examples
-
-This directory contains multiple examples that explain the usage of the GraylogAPI:
-
-
-
-- [How to create stream](create_stream.rb)
-
--- lib/graylogapi.rb
+++ lib/graylogapi.rb
@@ -1,55 +1,7 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-require 'graylogapi/alerts'
-require 'graylogapi/client'
-require 'graylogapi/dashboards'
-require 'graylogapi/static_fields'
-require 'graylogapi/streams'
-require 'graylogapi/system'
-require 'graylogapi/users'
 require 'graylogapi/version'
 
-# class for work with graylog api
 class GraylogAPI
-  # @return [GraylogAPI::Client]
-  attr_reader :client
-
-  # Initializes a new GraylogAPI object
-  #
-  # @param options [Hash]
-  # @return [GraylogAPI]
-  def initialize(options = {})
-    @client = Client.new(options)
-  end
-
-  # @return [GraylogAPI::Alerts]
-  def alerts
-    @alerts ||= Alerts.new(@client)
-  end
-
-  # @return [GraylogAPI::System]
-  def system
-    @system ||= System.new(@client)
-  end
-
-  # @return [GraylogAPI::Streams]
-  def streams
-    @streams ||= Streams.new(@client)
-  end
-
-  # @return [GraylogAPI::Dashboards]
-  def dashboards
-    @dashboards ||= Dashboards.new(@client)
-  end
-
-  # @return [GraylogAPI::StaticFields]
-  def static_fields
-    @static_fields ||= StaticFields.new(@client)
-  end
-
-  # @return [GraylogAPI::Users]
-  def users
-    @users ||= Users.new(@client)
-  end
 end
